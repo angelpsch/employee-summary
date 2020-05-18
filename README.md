39193cbf4469bf45afa23e651643a8478592fa06
angelpsch <angelpsch@users.noreply.github.com>
2020-05-18 07:48:27 -0500

    err fix

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Employee Information Application
 
 ## [Link to Deployed Application](https://angelpsch.github.io/employee-summary/)
+
+### Description:
+- Generate a webpage that displays each member of your team's info, such as: 
+    - Name
+    - E-mail
+    - Role
+- For each role, have special added information, for example: 
+    - Managers information will include their office number
+    - Engineers information will include their GitHub account
+    - Interns informtaion will include the school they're attending
+
+### Instructions: 
+
+- In the Node CLI, you will enter each employee's information when prompted.
+- When you are done, you will tell it that there are no more employees you would like to add.
+- This will generate your HTML page with all of the information on it.
--- app.js
+++ app.js
@@ -1,46 +1,160 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const OUTPUT_DIR = path.resolve(__dirname, 'output');
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+const render = require('./lib/htmlRenderer');
 
-const employees = [
-    {
-        employee: {
-            getName(){
-                return inquirer
-                    .prompt(
-                        {
-                            type: 'input',
-                            name: 'name',
-                            message: 'What is your { role }\'s name?'
-                        }
-                    )
-            },
-            getEmail(){
-                return inquirer 
-                    .prompt(
-                        {
-                            type: 'input',
-                            name: 'email',
-                            message: 'What is your { role }\'s email?'
-                        }
-                    )
-            }
+
+let employees = []; 
+let addMember = true; 
+let addManager = true; 
+
+function newEmployee() {
+    return inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'role',
+        message: 'What role does this employee hold?',
+        choices: ['Intern', 'Engineer', 'Manager', 'No more employees to add'],
+      },
+    ])  
+    .then((res) => {
+        switch (res.role) {
+            case 'Manager':
+                return createManager();
+            case 'Engineer':
+                return createEngineer();
+            case 'Intern':
+                return createIntern(); 
+            case 'No more employees to add': 
+                addMember = false;
+                break;  
         }
+    })
+}
+
+
+function createManager(){
+    return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+      },
+    ])
+    .then((res) => {
+      employee = new Manager(res.name, res.id, res.email, res.officeNumber);
+      employees.push(employee);
+    });
+}
+function createEngineer(){
+    return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub?",
+      },
+    ])
+    .then((res) => {
+      employee = new Engineer(res.name, res.id, res.email, res.github);
+      employees.push(employee);
+    });
+}
+function createIntern(){
+    return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the intern's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?",
+      },
+    ])
+    .then((res) => {
+      employee = new Intern(res.name, res.id, res.email, res.school);
+      employees.push(employee);
+    });
+}
+
+
+
+async function uniteTeam() {
+    while(addMember === true){
+        if(addManager == true){
+            await createManager(); 
+
+            addManager = false; 
+        } else {
+             await newEmployee(); 
+        }
+    } 
+
+    const html = render(employees); 
+
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
     }
-]
 
+    fs.writeFile(outputPath, html, (err) => {
+        if (err) throw err; 
+        console.log('Your team is now united... in HTML. Good luck.');
+    });
+}
 
-render(employees); 
-
-
+uniteTeam(); 
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
